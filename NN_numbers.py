0f65d1621109a63aeb920e6b0aa4b978d424f7e4
AmirRazavi121303 <amirsh.razavi@gmail.com>
2025-07-04 00:23:08 +0200

    learning

--- NN_numbers.py
+++ NN_numbers.py
@@ -1,9 +1,19 @@
 import numpy as np
 import pandas as pd
 
-data = np.array(pd.read_csv(""))
+train_data = np.array(pd.read_csv("/Users/amir/Downloads/CodeAmir/NeuralNets/MNIST as csv/mnist_train.csv")).T
 
-r, c = data.shape #r for rows, c for columns
+test_data = np.array(pd.read_csv("/Users/amir/Downloads/CodeAmir/NeuralNets/MNIST as csv/mnist_test.csv")).T
 
-train_data = data[0:1000].T
+r_1, c_1 = train_data.shape #r for rows, c for columns
+r_2, c_2 = test_data.shape
 
+y_train = train_data[0]
+x_train = train_data[1:r_1]
+
+y_test = test_data[0]
+x_test = test_data[1:r_2]
+
+print(r_1,c_1, r_2, c_2)
+
+#the entire row, 0th column
