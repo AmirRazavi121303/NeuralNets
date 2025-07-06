b6571db398dcbc4fb2faac532b4ad73fe88bbd31
AmirRazavi121303 <amirsh.razavi@gmail.com>
2025-07-06 23:15:40 +0200

    progressing

--- NN_numbers.py
+++ NN_numbers.py
@@ -15,9 +15,9 @@ y_test = test_data[:, 0]
 x_test = test_data[:, 1:]
 
 def init_params(): #initializing weights and biases, weights are initally random but then optimized with backpropagation, biases are an array of zeroes, but get optimized later
-    W1 = np.random.randn(784, 64) * 0.01
+    W1 = np.random.rand(784, 64) * 0.01
     b1 = np.zeroes(1,64)
-    W2 = np.random.randn(64, 10) * 0.01
+    W2 = np.random.rand(64, 10) * 0.01
     b2 = np.zeroes(1,10)
     return W1, b1, W2, b2
 
