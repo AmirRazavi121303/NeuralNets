b521a4ccaf762846833552b01615583c9c0f9f9f
AmirRazavi121303 <amirsh.razavi@gmail.com>
2025-07-26 14:02:37 +0200

    finishing up

--- NN_numbers.py
+++ NN_numbers.py
@@ -24,10 +24,10 @@ print(x_train[0:10])
 #step 1: set matrices with random weights and biases
 def init_params(): 
     W1 = np.random.rand(64, 784) * 0.01 #or -0.5
-    b1 = np.zeroes(64, 1) #can also be random.rand
+    B1 = np.zeroes(64, 1) #can also be random.rand
     W2 = np.random.rand(10, 64) * 0.01 
-    b2 = np.zeroes(10, 1)
-    return W1, b1, W2, b2
+    B2 = np.zeroes(10, 1)
+    return W1, B1, W2, B2
 
 def ReLU(Z): #for fun try implementing different types of sigmoid functions
     return np.maximum(0, Z) #goes thru every value of the array (Z) if Z > 0 
@@ -35,10 +35,10 @@ def ReLU(Z): #for fun try implementing different types of sigmoid functions
 def softmax(Z):
     return np.exp(Z) / np.sum(np.exp(Z))
 
-def forward_prop(W1, b1, W2, b2, X):
-    Z1 = W1.dot(X) + b1
+def forward_prop(W1, B1, W2, B2, X):
+    Z1 = W1.dot(X) + B1
     A1 = ReLU(Z1)
-    Z2 = W2.dot(A1) + b2
+    Z2 = W2.dot(A1) + B2
     A2 = softmax(Z2)
     return Z1, A1, Z2, A2
 
@@ -52,7 +52,20 @@ def back_prop(W1, B1, W2, B2, Z1, A1, Z2, A2, X, Y):
     DZ2 = A2 - one_hot_Y
     DW2 = 1 / m * DZ2.dot(A1.T)
     DB2 = 1 / m * np.sum(DZ2)
-    DW1 = 1/ m * W2.T.dot(DZ2) * 
-    DB1 =
-    DZ1 = 
-    return
+    DZ1 = W2.T.dot(DZ2)
+    DW1 = 1 / m * DZ1.dot(X.T)
+    DB1 = 1 / m * np.sum(DZ1)
+    return DW1, DB1, DW2, DB2
+
+def update_params(W1, B1, W2, B2, DW1, DB1, DW2, DB2, alpha): #alpha = learning rate
+    W1 = W1 - alpha * DW1
+    B1 = B1 - alpha * DB1
+    W2 = W2 - alpha * DW2
+    B2 = B2 - alpha * DB2
+    return W1, B1, W2, B2
+
+def gradient_descent(X, Y, alpha, epochs):
+    W1, B1, W2, B2 = init_params()
+    Z1, A1, Z2, A2 = forward_prop(W1, B1, W2, B2, X)
+    DW1, DB1, DW2, DB2 = back_prop(W1, B1, W2, B2, Z1, A1, Z2, A2, X, Y)
+    W1, B1, W2, B2 = update_params(W1, B1, W2, B2, DW1, DB1, DW2, DB2, alpha)
